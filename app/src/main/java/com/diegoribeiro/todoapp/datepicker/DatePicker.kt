3d45751415effdf7719e50380894ef9904e677ed
andersonluisp <andersonluiscp@gmail.com>
2021-05-15 16:52:44 -0300

    Add class DatePicker.kt and implant Datepicker to AddFragment.kt

---
+++ app/src/main/java/com/diegoribeiro/todoapp/datepicker/DatePicker.kt
@@ -0,0 +1,71 @@
+package com.diegoribeiro.todoapp.datepicker
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.util.Log
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TimePicker
+import java.util.*
+
+class DatePicker(
+    editText: EditText?,
+    context: Context
+):  DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    var requireContext = context
+    var deadliteEditText = editText
+
+    var day: Int = 0
+    var month: Int = 0
+    var year: Int = 0
+    var hour: Int = 0
+    var minute: Int = 0
+
+    var savedDay: Int = 0
+    var savedMonth: Int = 0
+    var savedYear: Int = 0
+    var savedHour: Int = 0
+    var savedMinute: Int = 0
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month + 1
+        savedYear = year
+
+        getDateTimeCalendar()
+        TimePickerDialog(requireContext, this, hour,minute,true).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+
+        deadliteEditText?.setText(
+            "${String.format("%02d", savedDay)}-" +
+                    "${String.format("%02d", savedMonth)}-" +
+                    "${String.format("%04d", savedYear)} " +
+                    "${String.format("%02d", savedHour)}:" +
+                    "${String.format("%02d", savedMinute)}"
+        )
+    }
+
+    fun pickDate() {
+        deadliteEditText?.setOnClickListener{
+            Log.w("test click", "Deadline Clicked")
+            getDateTimeCalendar()
+            DatePickerDialog(requireContext, this, year, month, day).show()
+        }
+    }
+
+    private fun getDateTimeCalendar(){
+        val cal = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+}
--- app/src/main/java/com/diegoribeiro/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/diegoribeiro/todoapp/fragments/add/AddFragment.kt
@@ -2,6 +2,7 @@ package com.diegoribeiro.todoapp.fragments.add
 
 import android.os.Bundle
 import android.view.*
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -10,10 +11,13 @@ import com.diegoribeiro.todoapp.R
 import com.diegoribeiro.todoapp.data.models.ToDoData
 import com.diegoribeiro.todoapp.data.viewmodel.SharedViewModel
 import com.diegoribeiro.todoapp.data.viewmodel.ToDoViewModel
+import com.diegoribeiro.todoapp.datepicker.DatePicker
 import kotlinx.android.synthetic.main.fragment_add.*
 import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
+
+    var deadline: EditText? = null
 
     private val mToDoViewModel: ToDoViewModel by viewModels()
     private val mSharedViewModel: SharedViewModel by viewModels()
@@ -22,9 +26,12 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         setHasOptionsMenu(true)
         val view =  inflater.inflate(R.layout.fragment_add, container, false)
+        deadline = view.findViewById(R.id.deadline_et)
+        DatePicker(deadline, requireContext()).pickDate()
 
         setHasOptionsMenu(true)
         view.priorities_spinner.onItemSelectedListener = mSharedViewModel.listener
@@ -47,11 +54,12 @@ class AddFragment : Fragment() {
         val mTitle = title_et.text.toString()
         val mPriority = priorities_spinner.selectedItemPosition
         val mDescription = description_et.text.toString()
+        val mDeadline = deadline?.text.toString()
 
         val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
         if (validation){
             val newData = ToDoData(0, mTitle, mSharedViewModel
-                    .parseIntToPriority(mPriority), mDescription)
+                    .parseIntToPriority(mPriority), mDescription, mDeadline)
             mToDoViewModel.insert(newData)
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
             Toast.makeText(requireContext(), R.string.saved_successfully, Toast.LENGTH_SHORT).show()
