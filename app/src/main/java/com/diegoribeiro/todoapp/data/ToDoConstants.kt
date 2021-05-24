2d75895d15de0dda922d90cc73f2fa55d5abd5ad
andersonluisp <andersonluiscp@gmail.com>
2021-05-23 23:22:57 -0300

    created ToDoConstants to save the const val of project

---
+++ app/src/main/java/com/diegoribeiro/todoapp/data/ToDoConstants.kt
@@ -0,0 +1,14 @@
+package com.diegoribeiro.todoapp.data
+
+class ToDoConstants {
+
+    companion object{
+        const val EXTRA_TASK_NAME = "EXTRA_TASK_NAME"
+        const val EXTRA_TASK_ID = "EXTRA_TASK_ID"
+        const val EXTRA_TASK_PRIORITY = "EXTRA_TASK_PRIORITY"
+        const val EXTRA_TASK_DEADLINE = "EXTRA_TASK_DEADLINE"
+        const val EXTRA_TAG = "EXTRA_TAG"
+        const val SCHEDULE_EXTRA_TASK_NAME = "SCHEDULE_EXTRA_TASK_NAME"
+    }
+
+}
