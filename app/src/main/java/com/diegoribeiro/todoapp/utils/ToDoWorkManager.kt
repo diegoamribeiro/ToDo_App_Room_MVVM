6f0cae343cce2dd011ff27b39b8899f347f7e82d
andersonluisp <andersonluiscp@gmail.com>
2021-06-01 10:09:30 -0300

    Create class ToDoWorkManager.kt

---
+++ app/src/main/java/com/diegoribeiro/todoapp/utils/ToDoWorkManager.kt
@@ -0,0 +1,70 @@
+package com.diegoribeiro.todoapp.utils
+
+import android.content.Context
+import android.os.Build
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.viewModels
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.diegoribeiro.todoapp.R
+import com.diegoribeiro.todoapp.data.ToDoConstants
+import com.diegoribeiro.todoapp.data.models.Priority
+import com.diegoribeiro.todoapp.data.models.ToDoData
+import com.diegoribeiro.todoapp.data.models.ToDoDateTime
+import com.diegoribeiro.todoapp.data.viewmodel.SharedViewModel
+import kotlinx.android.synthetic.main.fragment_add.view.*
+import java.time.OffsetDateTime
+import java.time.temporal.ChronoUnit
+import java.util.concurrent.TimeUnit
+
+@RequiresApi(Build.VERSION_CODES.O)
+class ToDoWorkManager(val workManager: WorkManager) {
+
+    private var deadLine: ToDoDateTime = ToDoDateTime()
+
+    fun createWorkManager(toDoData: ToDoData, view: View, daysToReminder: Long, hoursToReminder: Long){
+        deadLine = ToDoDateTime(
+            toDoData.dateTime!!.dayOfMonth,
+            toDoData.dateTime!!.monthValue,
+            toDoData.dateTime!!.year,
+            toDoData.dateTime!!.hour,
+            toDoData.dateTime!!.minute
+        )
+
+        val timeTilFuture = ChronoUnit.MILLIS.between(
+            OffsetDateTime.now(),
+            toDoData.dateTime?.minusDays(daysToReminder)?.minusHours(hoursToReminder)
+        )
+        val data = Data.Builder()
+        val stringPriority = parsePriorityToString(view, toDoData.priority)
+        val stringDeadLine =
+            " ${view.context.getString(R.string.deadline)}: " +
+                    "${deadLine.getDate()} " +
+                    "${deadLine.getTime()}"
+
+        data.putString(ToDoConstants.EXTRA_TASK_NAME, toDoData.title)
+        data.putString(ToDoConstants.EXTRA_TASK_PRIORITY, stringPriority)
+        data.putString(ToDoConstants.EXTRA_TASK_DEADLINE, stringDeadLine)
+        data.putInt(ToDoConstants.EXTRA_TASK_ID, toDoData.id)
+
+        val workRequest = OneTimeWorkRequest.Builder(NotificationWorkManager::class.java)
+            .setInitialDelay(timeTilFuture, TimeUnit.MILLISECONDS)
+            .setInputData(data.build())
+            .addTag(toDoData.title)
+            .build()
+
+        workManager.enqueue(workRequest)
+    }
+
+    private fun parsePriorityToString(view: View, priority: Priority): String {
+        val arrayStringPriorities = view.context.resources.getStringArray(R.array.priorities)
+        return when (priority) {
+            Priority.HIGH -> arrayStringPriorities[0]
+            Priority.MEDIUM -> arrayStringPriorities[1]
+            Priority.LOW -> arrayStringPriorities[2]
+        }
+    }
+
+}
